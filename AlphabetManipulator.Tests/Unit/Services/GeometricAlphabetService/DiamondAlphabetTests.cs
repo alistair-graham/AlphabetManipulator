2f80d8a8d63273937fbc58a5380062476ca11049
Annette <annette.graham@sky.uk>
2023-02-05 22:18:30 +0000

    [Alistair] Test that CreateFromLetter throws exception if argument is not a letter

--- AlphabetManipulator.Tests/Unit/Services/GeometricAlphabetService/DiamondAlphabetTests.cs
+++ AlphabetManipulator.Tests/Unit/Services/GeometricAlphabetService/DiamondAlphabetTests.cs
@@ -1,6 +1,7 @@
 ﻿using Xunit;
 using Moq;
 using AlphabetManipulator.Services.GeometricAlphabetService;
+using System.Diagnostics.Metrics;
 
 namespace AlphabetManipulator.Tests.Unit.Services.GeometricAlphabetService;
 
@@ -20,7 +21,7 @@ public class DiamondAlphabetServiceTests
     {
         const char LETTER_A = 'A';
 
-        var result = _diamondAlphabetService.CreateFromChar(LETTER_A);
+        var result = _diamondAlphabetService.CreateFromLetter(LETTER_A);
 
         Assert.Equal("A", result);
     }
@@ -30,7 +31,7 @@ public class DiamondAlphabetServiceTests
     {
         const char LETTER_M = 'M';
 
-        var result = _diamondAlphabetService.CreateFromChar(LETTER_M);
+        var result = _diamondAlphabetService.CreateFromLetter(LETTER_M);
 
         var expectedDiamond = "            A            \n           B B           \n          C   C          \n         D     D         \n        E       E        \n       F         F       \n      G           G      \n     H             H     \n    I               I    \n   J                 J   \n  K                   K  \n L                     L \nM                       M\n L                     L \n  K                   K  \n   J                 J   \n    I               I    \n     H             H     \n      G           G      \n       F         F       \n        E       E        \n         D     D         \n          C   C          \n           B B           \n            A            ";
         Assert.Equal(expectedDiamond, result);
@@ -41,7 +42,7 @@ public class DiamondAlphabetServiceTests
     {
         const char LETTER_Z = 'Z';
 
-        var result = _diamondAlphabetService.CreateFromChar(LETTER_Z);
+        var result = _diamondAlphabetService.CreateFromLetter(LETTER_Z);
 
         var expectedDiamond = "                         A                         \n                        B B                        \n                       C   C                       \n                      D     D                      \n                     E       E                     \n                    F         F                    \n                   G           G                   \n                  H             H                  \n                 I               I                 \n                J                 J                \n               K                   K               \n              L                     L              \n             M                       M             \n            N                         N            \n           O                           O           \n          P                             P          \n         Q                               Q         \n        R                                 R        \n       S                                   S       \n      T                                     T      \n     U                                       U     \n    V                                         V    \n   W                                           W   \n  X                                             X  \n Y                                               Y \nZ                                                 Z\n Y                                               Y \n  X                                             X  \n   W                                           W   \n    V                                         V    \n     U                                       U     \n      T                                     T      \n       S                                   S       \n        R                                 R        \n         Q                               Q         \n          P                             P          \n           O                           O           \n            N                         N            \n             M                       M             \n              L                     L              \n               K                   K               \n                J                 J                \n                 I               I                 \n                  H             H                  \n                   G           G                   \n                    F         F                    \n                     E       E                     \n                      D     D                      \n                       C   C                       \n                        B B                        \n                         A                         ";
         Assert.Equal(expectedDiamond, result);
@@ -52,7 +53,7 @@ public class DiamondAlphabetServiceTests
     {
         const char LOWERCASE_B = 'b';
 
-        var result = _diamondAlphabetService.CreateFromChar(LOWERCASE_B);
+        var result = _diamondAlphabetService.CreateFromLetter(LOWERCASE_B);
 
         var expectedDiamond = @" A 
 B B
@@ -61,12 +62,13 @@ B B
     }
 
     [Fact]
-    public void CreateFromChar_NonAlphabeticalCharacter_ReturnsEmptyString()
+    public void CreateFromChar_NonAlphabeticalCharacter_ThrowsArgumentOutOfBoundsException()
     {
-        const char NON_ALPHABETICAL_CHARACTER = '?';
+        const char NON_ALPHABETICAL_CHARACTER = ']';
 
-        var result = _diamondAlphabetService.CreateFromChar(NON_ALPHABETICAL_CHARACTER);
+        var action = () => _diamondAlphabetService.CreateFromLetter(NON_ALPHABETICAL_CHARACTER);
 
-        Assert.Equal(string.Empty, result);
+        var exception = Assert.Throws<ArgumentOutOfRangeException>(action);
+        Assert.Equal("Argument must be a letter, instead was <]> (Parameter 'highestLetter')", exception.Message);
     }
 }
